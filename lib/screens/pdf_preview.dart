d109b54c6eb9fe1c15baa66036e042803e2517c5
arundev18 <arundev@hutechsolutions.com>
2023-11-02 17:13:02 +0530

    multi select user

--- lib/screens/pdf_preview.dart
+++ lib/screens/pdf_preview.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
+
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart';
-import 'package:pdf/widgets.dart' as pw;
 
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:stock_check/config/size_config.dart';
@@ -11,14 +11,16 @@ import 'package:stock_check/const/app_color.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:stock_check/const/app_text_style.dart';
+import 'package:stock_check/model/user_model.dart';
 import 'package:stock_check/provider/product_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:document_file_save_plus/document_file_save_plus.dart';
+import 'package:stock_check/provider/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 import '../widget/snack_bar.dart';
 import 'package:share_plus/share_plus.dart';
-
-var filePath = "";
 
 class CustomPDFViewScreen extends StatefulWidget {
   final List<ProductModel> item;
@@ -31,6 +33,7 @@ class CustomPDFViewScreen extends StatefulWidget {
 
 class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
   String? pdfPath;
+  List<User> selectedNumbers = [];
 
   @override
   void initState() {
@@ -39,7 +42,6 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
     generateCustomPDF().then((path) {
       setState(() {
         pdfPath = path;
-        filePath = pdfPath!;
       });
     });
   }
@@ -82,11 +84,11 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
                         verticalRadius: 5.0,
                         horizontalRadius: 5.0,
                         child: pw.Container(
-                            margin: pw.EdgeInsets.only(bottom: 6.0),
+                            margin: const pw.EdgeInsets.only(bottom: 6.0),
                             decoration: pw.BoxDecoration(
                               borderRadius: pw.BorderRadius.circular(5.0),
                               color: PdfColors.grey100,
-                              boxShadow: [
+                              boxShadow: const [
                                 pw.BoxShadow(
                                   color: PdfColors.grey, //(x,y)
                                   blurRadius: 12.0,
@@ -110,11 +112,10 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
                                             ),
                                             fit: pw.BoxFit.fitWidth)
                                         : pw.Stack(children: [
-                                      pw.Text("Image not added",
-                                          style: pw.TextStyle(
-                                              fontSize:
-                                              18))
-                                    ]), /*pw.Image(
+                                            pw.Text("Image not added",
+                                                style: const pw.TextStyle(
+                                                    fontSize: 18))
+                                          ]), /*pw.Image(
                                         pw.MemoryImage(
                                            File("assets/images/no_image.png")
                                                 .readAsBytesSync(),
@@ -124,7 +125,8 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
                                   pw.Expanded(
                                       flex: 3,
                                       child: pw.Padding(
-                                        padding: pw.EdgeInsets.only(left: 20),
+                                        padding:
+                                            const pw.EdgeInsets.only(left: 20),
                                         child: pw.Column(
                                             crossAxisAlignment:
                                                 pw.CrossAxisAlignment.start,
@@ -132,39 +134,28 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
                                                 .MainAxisAlignment.spaceAround,
                                             children: [
                                               pw.Text(
-                                                  "Name : " +
-                                                      widget.item[index]
-                                                          .productName,
-                                                  style: pw.TextStyle(
+                                                  "Name : ${widget.item[index].productName}",
+                                                  style: const pw.TextStyle(
                                                       fontSize: 18)),
                                               pw.Text(
-                                                  "Category : " +
-                                                      widget
-                                                          .item[index].category,
-                                                  style: pw.TextStyle(
+                                                  "Category : ${widget.item[index].category}",
+                                                  style: const pw.TextStyle(
                                                       fontSize: 18)),
                                               pw.Text(
-                                                  "Price : " +
-                                                      widget.item[index]
-                                                          .productPrice,
-                                                  style: pw.TextStyle(
+                                                  "Price : ${widget.item[index].productPrice}",
+                                                  style: const pw.TextStyle(
                                                       fontSize: 18)),
                                               pw.Text(
-                                                  "Description : " +
-                                                      widget.item[index]
-                                                          .description,
-                                                  style: pw.TextStyle(
+                                                  "Description : ${widget.item[index].description}",
+                                                  style: const pw.TextStyle(
                                                       fontSize: 18)),
                                               pw.Text(
-                                                  "Code : " +
-                                                      widget.item[index].code,
-                                                  style: pw.TextStyle(
+                                                  "Code : ${widget.item[index].code}",
+                                                  style: const pw.TextStyle(
                                                       fontSize: 18)),
                                               pw.Text(
-                                                  "Size : " +
-                                                      widget.item[index]
-                                                          .packagesize,
-                                                  style: pw.TextStyle(
+                                                  "Size : ${widget.item[index].packagesize}",
+                                                  style: const pw.TextStyle(
                                                       fontSize: 18)),
                                             ]),
                                       )),
@@ -275,7 +266,7 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
             heroTag: "share",
             backgroundColor: green33,
             onPressed: () {
-              sharePdfOnWhatsApp();
+              showList();
             },
             child: Icon(
               Icons.share_outlined,
@@ -283,7 +274,7 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
               size: 30 * SizeConfig.widthMultiplier!,
             ),
           ),
-          SizedBox(width: 10),
+          const SizedBox(width: 10),
           FloatingActionButton(
             heroTag: "download",
             elevation: 0,
@@ -318,12 +309,76 @@ class _CustomPDFViewScreenState extends State<CustomPDFViewScreen> {
             ),
     );
   }
+
+  void showList() async{
+    await showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (_) => Dialog(
+          insetPadding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 2.0),
+          backgroundColor: Colors.transparent,
+          child:FutureBuilder(
+                future: Provider.of<UserProvider>(context, listen: false).getAllUser(),
+                  builder: (context, snapshot) =>
+                  snapshot.connectionState == ConnectionState.waiting
+                      ? const Center(child: CircularProgressIndicator())
+                      : Consumer<UserProvider>(
+                    child: Center(
+                      child: Text(
+                        'No users added',
+                        textAlign: TextAlign.center,
+                        style: AppTextStyle.content,
+                      ),
+                    ),
+                      builder: (context, userProvider, child) =>
+                      userProvider.allUsers.isEmpty
+                          ? child!
+                          :MultiSelectDialog(
+                        width: 125 * SizeConfig.widthMultiplier!,
+                        height: 340 * SizeConfig.heightMultiplier!,
+                        listType: MultiSelectListType.LIST,
+                        initialValue: selectedNumbers,
+                        separateSelectedItems: false,
+                        backgroundColor: Colors.white,
+                        items: userProvider.allUsers
+                            .map((element) =>
+                            MultiSelectItem<User>(
+                                element,
+                                element.name! +
+                                    "\n" +
+                                    element.whatsAppNo!))
+                            .toList(),
+                        itemsTextStyle: AppTextStyle.content.copyWith(
+                          fontSize: 14 * SizeConfig.textMultiplier!,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'TT Commons',
+                        ),
+                        selectedItemsTextStyle:
+                        AppTextStyle.content.copyWith(
+                          fontSize: 14 * SizeConfig.textMultiplier!,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'TT Commons',
+                        ),
+                        confirmText: const Text("Share"),
+                        cancelText:const Text("Cancel"),
+                        title: const Text("Select Users"),
+                        selectedColor: Colors.black,
+                        searchable: false,
+                        onConfirm: (results) {
+                          selectedNumbers = results;
+                        },
+                      )
+                  )
+              )
+        ));
+  }
+
 }
 
-void sharePdfOnWhatsApp() async {
+void sharePdfOnWhatsApp(String path) async {
   // Define the message you want to send (optional).
   String message = "Check out this PDF!";
 
   // Share the PDF file using WhatsApp.
-  await Share.shareFiles([filePath], text: message);
+  await Share.shareFiles([path], text: message);
 }
